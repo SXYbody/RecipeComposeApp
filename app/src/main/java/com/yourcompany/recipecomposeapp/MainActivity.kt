5326f61c034857e8d7996ecb7806fecfaba4e22b
saveliy <saveliykislic88@mail.ru>
2026-01-23 20:31:31 +0700

    RCA-1-5-add-text

--- app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
@@ -17,14 +20,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            RecipeComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                Scaffold(
+                    content = { paddingValues ->
+                        Text(
+                            text = "Recipes App",
+                            modifier = Modifier.padding(paddingValues),
+                        )
+                    }
+                )
             }
         }
     }
