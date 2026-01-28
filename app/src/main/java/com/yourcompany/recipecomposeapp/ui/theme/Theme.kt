860743488a7e2f89f0ef80650bf3ab2b319eef00
saveliy <saveliykislic88@mail.ru>
2026-01-28 14:53:43 +0700

    RCA-06-take-edits

--- app/src/main/java/com/yourcompany/recipecomposeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.yourcompany.recipecomposeapp.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 val PrimaryColor = Color(0xFF5F3678) // Фиолетовый для заголовков и акцентов
 val AccentColor = Color(0xFFE13E3E) // Красный для кнопки избранного
