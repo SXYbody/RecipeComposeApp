0cdbeda4a0a2d9c4795af2168784c56da12cb109
saveliy <saveliykislic88@mail.ru>
2026-01-31 12:15:10 +0700

    RCA-07-optimize-imports

--- app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
@@ -1,6 +1,5 @@
 package com.yourcompany.recipecomposeapp
 
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
