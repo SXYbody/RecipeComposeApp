3062164c5a46c90bd0e722640ccfeac5093a6e31
saveliy <saveliykislic88@mail.ru>
2026-02-01 10:43:17 +0700

    Revert "RCA-07-optimize-imports"
    This reverts commit 0cdbeda4a0a2d9c4795af2168784c56da12cb109.

--- app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
@@ -1,5 +1,6 @@
 package com.yourcompany.recipecomposeapp
 
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
