a3e66f1c8e422fa7d26663f17f5595790531afa4
saveliy <saveliykislic88@mail.ru>
2026-02-21 10:34:51 +0700

    RCA-19-add-deep-links

--- app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
@@ -1,26 +1,45 @@
 package com.yourcompany.recipecomposeapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+    private var deepLinkIntent by mutableStateOf<Intent?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        intent?.data?.let { _ ->
+            deepLinkIntent = intent
+        }
+
         setContent {
-            RecipesApp()
+            RecipesApp(
+                intent = deepLinkIntent
+            )
         }
     }
-}
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        intent.data?.let { _ ->
+            deepLinkIntent = intent
+        }
+        setIntent(intent)
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
--- app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
@@ -1,10 +1,12 @@
 package com.yourcompany.recipecomposeapp
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,13 +29,18 @@ import com.yourcompany.recipecomposeapp.ui.navigation.Destination
 import com.yourcompany.recipecomposeapp.ui.recipes.RecipesScreen
 import com.yourcompany.recipecomposeapp.ui.recipes.model.RecipeUiModel
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
-fun RecipesApp() {
+fun RecipesApp(
+    intent: Intent? = null
+) {
     RecipeComposeAppTheme() {
         var selectedCategoryTitle by remember { mutableStateOf<String?>(null) }
         val navController = rememberNavController()
+
+        AppNavHost(navController = navController, deepLinkIntent = intent)
 
         Scaffold(
             content = { paddingValues ->
@@ -130,3 +138,27 @@ fun RecipesApp() {
     }
 }
 
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    deepLinkIntent: Intent? = null
+) {
+    LaunchedEffect(deepLinkIntent) {
+        deepLinkIntent?.data?.let { uri ->
+            val recipeId: Int? = when (uri.scheme) {
+                "recipeapp" ->
+                    if (uri.host == "recipe") uri.pathSegments[0].toIntOrNull() else null
+
+                "https", "http" ->
+                    if (uri.pathSegments[0] == "recipe") uri.pathSegments[1].toIntOrNull() else null
+
+                else -> null
+            }
+
+            if (recipeId != null) {
+                delay(100)
+                navController.navigate(Destination.Recipes.createRoute(recipeId))
+            }
+        }
+    }
+}
