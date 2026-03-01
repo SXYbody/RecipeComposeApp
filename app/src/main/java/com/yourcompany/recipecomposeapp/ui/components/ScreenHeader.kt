2bfae838b247bda403c4ac2ffbc2214eea1af080
saveliy <saveliykislic88@mail.ru>
2026-03-01 10:46:33 +0700

    RCA-20 take-edits

--- app/src/main/java/com/yourcompany/recipecomposeapp/ui/components/ScreenHeader.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/components/ScreenHeader.kt
@@ -1,10 +1,11 @@
 package com.yourcompany.recipecomposeapp.ui.components
 
-
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -45,12 +47,13 @@ fun ScreenHeader(
         Image(
             painter = painter,
             contentDescription = painterContent,
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Surface(
-            shape = RoundedCornerShape(10.dp)
+
+        Row(
+            modifier = Modifier.align(Alignment.TopEnd)
         ) {
-            Text(text = text)
             if (showShareButton) {
                 Button(
                     onClick = onShareClick,
@@ -81,6 +84,11 @@ fun ScreenHeader(
                 }
             }
         }
+
+        Surface(
+            modifier = Modifier.align(Alignment.BottomStart),
+            shape = RoundedCornerShape(10.dp)
+        ) { Text(text = text) }
     }
 }
 
